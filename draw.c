2da7bf8c7873a9632fff07ca57843b5717360ebe
JimmyLuo0 <jluo00@stuy.edu>
2020-02-10 11:14:29 -0500

    cleaned up code

--- draw.c
+++ draw.c
@@ -7,8 +7,6 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c){
-  //int A = y1-y0; dy
-  //int B = x0 - x1; -dx
   int dy, dx, leftx, lefty, rightx, righty;
   if(x0 == min(x0,x1)){
     dx = x1 - x0;
@@ -19,18 +17,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c){
     righty = y1;
   }
   else{
-    dx = x0 - x1;
-    dy = y0 - y1;
-    leftx = x1;
-    lefty = y1;
-    rightx = x0;
-    righty = y0;
-  }
-  if(dy == 0){
-    drawh(leftx,lefty,rightx,s,c);
-  }
-  if(dx == 0){
-    drawv(leftx,lefty, lefty, s, c);
+    draw_line(x1,y1,x0,y0,s,c);
   }
   if (dy <= dx && dy >= 0){
     draw_oct1(leftx, lefty, rightx, righty, s, c, dx, dy);
@@ -63,32 +50,42 @@ void drawv(int x0, int y0, int y1, screen s, color c){
 }
 
 void draw_oct1(int x0, int y0, int x1, int y1, screen s, color c, int dx, int dy){
-  int x = x0;
-  int y = y0;
-  int d = 2 * dy - dx;
-  while (x <= x1){
-    plot(s,c,x,y);
-    if(d>0){
-      y++;
-      d += -2*dx;
+  if(dy == 0){
+    drawh(x0,y0,x1,s,c);
+  }
+  else{
+    int x = x0;
+    int y = y0;
+    int d = 2 * dy - dx;
+    while (x <= x1){
+      plot(s,c,x,y);
+      if(d>0){
+        y++;
+        d += -2*dx;
+      }
+      x++;
+      d += 2*dy;
     }
-    x++;
-    d += 2*dy;
   }
 }
 
 void draw_oct2(int x0, int y0, int x1, int y1, screen s, color c, int dx, int dy){
-  int x = x0;
-  int y = y0;
-  int d = dy + 2 * (-dx);
-  while (y <= y1){
-    plot(s,c,x,y);
-    if(d<0){
-      x++;
-      d += 2*dy;
+  if(dx == 0){
+    drawv(x0,y0, y1, s, c);
+  }
+  else{
+    int x = x0;
+    int y = y0;
+    int d = dy + 2 * (-dx);
+    while (y <= y1){
+      plot(s,c,x,y);
+      if(d<0){
+        x++;
+        d += 2*dy;
+      }
+      y++;
+      d += -2*dx;
     }
-    y++;
-    d += -2*dx;
   }
 }
 
